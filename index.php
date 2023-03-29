c2acfc7deeef33e2696daf78905b6374d2950006
Sergio Paucara <sergiopaucara@gmail.com>
2023-03-29 12:28:59 -0400

    add index

--- index.php
+++ index.php
@@ -4,7 +4,21 @@ include "headAlg.php";
 
 ?>
 
+<h1 style = "text-align:center;">Generación de Números Pseudoaleatorios </h1>
 
+<div style = "padding:10% 10% 10% 10%">
+<h2>Los números Pseudoaleatorios:</h2>
+La generación de números en forma determinística se llaman
+pseudoaleatorios, las mismas se utilizan para la realización de una simulación que incluya la
+variabilidad dentro de sus eventos, es necesario generar una serie de números aleatorios, y que la
+misma se extrapole al modelo de simulación que se está construyendo, por tanto los números
+aleatorios juegan un papel importante en la construcción del modelo.
+
+<h2>Métodos de Generación de números pseudoaleatorios: </h2>
+
+Existen varios métodos para la
+generación de números peseudoaleatorios, y cada uno de ellos tiene su propio algoritmo.
+</div>
 
 <?php
 	// include("foot.php");
